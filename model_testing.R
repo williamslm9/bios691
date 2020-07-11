15fe02426a9eb294d3c449710139bbfc3ddc00c6
Laura <williamslm9@vcu.edu>
2020-07-11 14:40:58 -0400

    model updates

--- model_testing.R
+++ model_testing.R
@@ -4,18 +4,19 @@ library(tensorflow)
 library(dplyr)
 set.seed(8675309)
 train=read.csv('train.csv')
-tensorflow::tf$compat$v1$disable_eager_execution()
+# tensorflow::tf$compat$v1$disable_eager_execution()
 
-train$Description=iconv(train$Description,from='UTF-8',to='ASCII')
-
+train$Age=train$Age/12
+train$mixed_breed = ifelse(train$Breed2==0,0,1)
+train$fee = ifelse(train$Fee==0,0,1)
+train$color = ifelse(train$Color3==0 & train$Color2 == 0,0,
+                     ifelse(train$Color1!=0 & train$Color2!=0 &train$Color3==0,1,2))
 
 train1 = train %>% 
-  select(-Name,-RescuerID,-Description,-PetID,-State)
+  select(-Name,-RescuerID,-Description,-PetID,-State,-Breed1,-Breed2,-Fee,-Color1,-Color2,-Color3)
 
 sample_train = sample(nrow(train1))[1:round(.7*nrow(train1))]
 sample_test = sample(nrow(train1))[((round(.7*nrow(train1)))+1):nrow(train1)]
-
-
 
 train_df = train1[sample_train,]
 test_df = train1[sample_test,]
@@ -24,27 +25,21 @@ train_labels = train_df$AdoptionSpeed
 test_labels = test_df$AdoptionSpeed
 
 train_df = train_df %>% select(-AdoptionSpeed)
+test_df = test_df %>%  select(-AdoptionSpeed)
 
 train_df=as.matrix(train_df)
 dimnames(train_df)=NULL
-
-
-vectorize_sequences <- function(sequences, dimension = 8996) {
-  results <- matrix(0, nrow = length(sequences), ncol = dimension)
-  for (i in 1:length(sequences))
-    results[i, sequences[[i]]] <- 1
-  results
-}
-
-# train_df=vectorize_sequences(train_df)
-
+test_df=as.matrix(test_df)
+dimnames(test_df)=NULL
 
 train_labels=to_categorical(train_labels)
-
+test_labels = to_categorical(test_labels)
 
 model <- keras_model_sequential() %>% 
-  layer_dense(units = 64, activation = "relu", input_shape = c(18)) %>% 
-  layer_dense(units = 16, activation = "relu") %>% 
+  layer_dense(units = 512, activation = "relu", input_shape = (15)) %>% 
+  layer_dense(units = 256, activation = "relu") %>%
+  # layer_dense(units = 128, activation = "relu") %>%
+  # layer_dense(units = 64, activation = "relu") %>%
   layer_dense(units = 5, activation = "softmax")
 
 
@@ -54,19 +49,15 @@ model %>% compile(
   metrics = c("accuracy")
 )
 
-
-
 history <- model %>% fit(
   (train_df),
   (train_labels),
-  epochs = 50,
-  batch_size=5,
+  epochs = 100,
   validation_split=.2
 )
 
 
-
-
+model %>%  evaluate(test_df,test_labels)
 
 
 
